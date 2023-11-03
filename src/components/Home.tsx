6a382d7c1cc686e1ec4e93a856ae88b90b90bc41
Adam Janicki <adamjanicki2@gmail.com>
2023-11-02 20:31:26 -0400

    updates

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,7 +1,7 @@
 const Home = () => {
   return (
     <div className="flex flex-column pb3 ph3" style={{ height: "70vh" }}>
-      <h1 className="f1 tc">React Skeleton</h1>
+      <h1 className="f1 tc">React skeleton</h1>
       <p className="tc f4 fw4">
         You can search for <code>skeleton</code> in your editor to find all
         places where you should make your own changes.
