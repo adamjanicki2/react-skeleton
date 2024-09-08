91d2cabc09de0ef052a3bbc73b997266d94ab1a1
Adam Janicki <adamjanicki2@gmail.com>
2024-09-07 19:15:35 -0700

    handle body overflow

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,15 +1,14 @@
 import { Link } from "@adamjanicki/ui";
 
 const Footer = () => (
-  <footer className="pa5 flex bt b--moon-gray bw1 items-center justify-center w-100">
-    <p className="fw5 f5">
+  <footer className="pv5 ph2 flex bt b--moon-gray bw1 items-center justify-center w-100">
+    <p className="fw5 f5 tc">
       Created from{" "}
       <Link
         target="_blank"
         rel="noreferrer"
         className="link"
         to="https://github.com/adamjanicki2/react-skeleton"
-        // eslint-disable-next-line
       >
         React Skeleton
       </Link>
